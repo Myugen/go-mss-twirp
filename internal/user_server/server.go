907d6d3e8f73d53acfac1ef6380ba0e88ddf62ed
Miguel Cabrera <miguel.cabrera@system73.com>
2021-09-13 01:51:09 +0100

    feat: Add server implementation

---
+++ internal/user_server/server.go
@@ -0,0 +1,34 @@
+package user_server
+
+import (
+	"context"
+
+	"github.com/twitchtv/twirp"
+
+	pb "github.com/myugen/go-mss-twirp/rpc/user"
+)
+
+type server struct{}
+
+func (s *server) Create(ctx context.Context, in *pb.UserIn) (*pb.UserOut, error) {
+	if in.Username == "" {
+		return nil, twirp.NewError(twirp.InvalidArgument, "The username field can not be empty")
+	}
+	if in.Name == "" {
+		return nil, twirp.NewError(twirp.InvalidArgument, "The name field can not be empty")
+	}
+	if in.Mail == "" {
+		return nil, twirp.NewError(twirp.InvalidArgument, "The mail field can not be empty")
+	}
+
+	return &pb.UserOut{
+		Id:       "new",
+		Username: in.Username,
+		Mail:     in.Mail,
+		Name:     in.Name,
+	}, nil
+}
+
+func New() pb.User {
+	return &server{}
+}
